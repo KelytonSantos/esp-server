d0f40806bfc75684fde7ed69e5070a2ee7912aa0
Kelyton de Lucas <kelytonlucas@gmail.com>
2024-03-14 15:37:00 -0300

    trying to add some css(basic)

--- Web.cpp
+++ Web.cpp
@@ -4,13 +4,22 @@ Web::Web(WebServer &server):serv(server){
 }
 
 void Web::handleRoot(){
-	String htmlContent = "<!DOCTYPE html><html><head><title>ESP Web Server</title></head><body>";
-	htmlContent += "<h1>Hello from ESP32!</h1>";
-	htmlContent += "<p>This is a sample web page served from an ESP32.</p>";
-	htmlContent += "</body></html>";
-	serv.send(200, "text/html", htmlContent); // Envie a resposta para o cliente
+    String htmlContent = "<!DOCTYPE html><html><head><title>ESP Web Server</title>";
 
+    // Adicionando estilos
+    htmlContent += "<style>"
+                  "body { background-color: #0E11E8; font-family: Arial, sans-serif; }"
+                  "h1 { color: #333333; }"
+                  "p { color: #666666; }"
+                  "</style>";
+
+    htmlContent += "</head><body>";
+    htmlContent += "<h1>Hello from ESP8266!</h1>";
+    htmlContent += "<p>This is a sample web page served from an ESP8266.</p>";
+    htmlContent += "</body></html>";
+    serv.send(200, "text/html", htmlContent);
 }
+
 void Web::titlePage(const String title){
 	String content = "<h1>" + title + "</h1>";
     serv.send(200, "text/html", content);
@@ -33,7 +42,6 @@ void Web::formXHTML(String action) {
                    + "<input type=\"submit\" value=\"Enter\"/></form>";
     serv.send(200, "text/html", content);
 }
-
 
 void Web::calc(){
 	String content = serv.arg("word");
